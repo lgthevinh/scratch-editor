1c5096fae348ed52b87886fe099d213106ecfe0e
lgthevinh <everwellmax@gmail.com>
2026-06-20 09:46:47 +0700

    feat: update web favicon and icon

--- packages/scratch-gui/webpack.config.js
+++ packages/scratch-gui/webpack.config.js
@@ -185,7 +185,7 @@ const buildConfig = baseConfig.clone()
         ...commonHtmlWebpackPluginOptions,
         chunks: ['gui'],
         template: 'src/playground/index.ejs',
-        title: 'Scratch 3.0 GUI'
+        title: 'ThingBlock'
     }))
     .addPlugin(new HtmlWebpackPlugin({
         ...commonHtmlWebpackPluginOptions,
